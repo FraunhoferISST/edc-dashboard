d77722640dfba654d7958a81ce3b478054fe15a3
Tim Dahlmanns <tim.dahlmanns@isst.fraunhofer.de>
2025-08-20 19:34:13 +0200

    feat: new layout

--- src/app/cx-policy/components/policy-editor/policy-builder/policy-builder.component.ts
+++ src/app/cx-policy/components/policy-editor/policy-builder/policy-builder.component.ts
@@ -20,7 +20,7 @@
 
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Permission, PolicyConfiguration } from '../../../models/policy';
-import { NgFor, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { PermissionComponent } from './permission/permission.component';
 import { FormsModule } from '@angular/forms';
 
@@ -29,7 +29,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: 'policy-builder.component.html',
   styleUrls: ['policy-builder.component.css'],
   standalone: true,
-  imports: [NgFor, NgIf, PermissionComponent, FormsModule],
+  imports: [NgIf, PermissionComponent, FormsModule],
 })
 export class PolicyBuilderComponent {
   private _policyConfig!: PolicyConfiguration;
@@ -51,8 +51,6 @@ export class PolicyBuilderComponent {
 
   currentPermission?: Permission;
 
-  selectedPermissions: Permission[] = [];
-
   panelOpenState = true;
 
   addPermission() {
@@ -65,20 +63,14 @@ export class PolicyBuilderComponent {
     this.policyChange.emit(this.policyConfig);
   }
 
-  onPermissionSelectionChange(selection: Permission[]) {
-    if (selection != null && selection.length > 0) {
-      this.currentPermission = selection[0];
-    }
+  onPermissionSelectionChange(selection: Permission) {
+    this.currentPermission = selection;
   }
-  removePermission() {
-    if (this.currentPermission) {
-      this.policyConfig.policy.permissions = this.policyConfig.policy.permissions.filter(
-        item => item != this.currentPermission,
-      );
-
-      this.currentPermission = this.policyConfig.policy.permissions[0];
-
-      this.policyChange.emit(this.policyConfig);
+  removePermission(target: Permission) {
+    this.policyConfig.policy.permissions = this.policyConfig.policy.permissions.filter(item => item != target);
+    if (target == this.currentPermission) {
+      this.currentPermission = this.policyConfig.policy.permissions[0] ?? undefined;
     }
+    this.policyChange.emit(this.policyConfig);
   }
 }
